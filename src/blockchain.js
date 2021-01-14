9c686930e7d89c475605e2776ca438de7f2b5d79
Bartosz <bartosz@wisniewski.pl>
2021-01-14 22:30:37 +0100

    Added signatures and futher validations

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
 class Transaction {
 
@@ -6,6 +8,33 @@ class Transaction {
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+
+    calculateHash() {
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+
+    signTransaction(signingKey) {
+        if (signingKey.getPublic('hex') !== this.fromAddress) {
+            throw new Error('Can\'t sign transaction from other wallet');
+        }
+
+        const hashTx = this.calculateHash();
+        const signed = signingKey.sign(hashTx, 'base64');
+        this.signature = signed.toDER('hex');
+    }
+
+    isValid() {
+        if (this.fromAddress == null) {
+            return true;
+        }
+
+        if (!this.signature || this.signature.length === 0) {
+            throw new Error('Transaction signature is missing');
+        }
+
+        const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+        return publicKey.verify(this.calculateHash(), this.signature);
     }
 
 }
@@ -25,10 +54,19 @@ class Block {
     }
 
     mineBlock(difficulty) {
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
             this.nonce++;
             this.hash = this.calculateHash();
         }
+    }
+
+    hasValidTransactions() {
+        for (let transaction of this.transactions) {
+            if (!transaction.isValid()) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
@@ -51,26 +89,38 @@ class Blockchain {
     }
 
     minePendingTrasactions(miningRewardAddress) {
+        let rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
+        this.pendingTransactions.push(rewardTx);
+
         let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
         block.mineBlock(this.difficulty);
+
         this.chain.push(block);
         this.pendingTransactions = new Transaction(null, miningRewardAddress, this.miningReward);
     }
 
     addTransaction(transaction) {
+        if (!transaction.fromAddress || !transaction.toAddress) {
+            throw new Error('Transaction requires from and to address');
+        }
+
+        if (!transaction.isValid()) {
+            throw new Error('Transaction must be valid');
+        }
+
         this.pendingTransactions.push(transaction);
     }
 
     getBalanceOfAddress(address) {
         let balance = 0;
 
-        for(let block of this.chain) {
-            for(let transaction of block.transactions) {
-                if(transaction.fromAddress === address) {
+        for (let block of this.chain) {
+            for (let transaction of block.transactions) {
+                if (transaction.fromAddress === address) {
                     balance -= transaction.amount;
                 }
 
-                if(transaction.toAddress === address) {
+                if (transaction.toAddress === address) {
                     balance += transaction.amount;
                 }
             }
@@ -83,12 +133,16 @@ class Blockchain {
         for (let i = 1; i < this.chain.length; i++) {
             const current = this.chain[i];
             const previous = this.chain[i - 1];
+
+            if (!current.hasValidTransactions()) {
+                return false;
+            }
             
-            if(current.hash !== current.calculateHash()) {
+            if (current.hash !== current.calculateHash()) {
                 return false;
             }
 
-            if(current.previousHash !== previous.hash) {
+            if (current.previousHash !== previous.hash) {
                 return false;
             }
         }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
 const {Transaction, Blockchain} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
 const cherryCoin = new Blockchain();
 
-const tr1 = new Transaction('address1', 'address2', 25);
-const tr2 = new Transaction('address3', 'address2', 15);
+const addr1 = ec.genKeyPair();
+const addr2 = ec.genKeyPair();
+const addr3 = ec.genKeyPair();
+const addr4 = ec.genKeyPair();
+
+const tr1 = new Transaction(addr1.getPublic('hex'), addr2.getPublic('hex'), 25);
+const tr2 = new Transaction(addr3.getPublic('hex'), addr2.getPublic('hex'), 15);
+
+tr1.signTransaction(addr1);
+tr2.signTransaction(addr3);
 
 cherryCoin.addTransaction(tr1);
 cherryCoin.addTransaction(tr2);
 
 cherryCoin.minePendingTrasactions('address4');
 
-console.log('address1 balance: ' + cherryCoin.getBalanceOfAddress('address1'));
-console.log('address2 balance: ' + cherryCoin.getBalanceOfAddress('address2'));
-console.log('address3 balance: ' + cherryCoin.getBalanceOfAddress('address3'));
-console.log('address4 balance: ' + cherryCoin.getBalanceOfAddress('address4'));
+console.log(addr1.getPublic('hex') + ' balance: ' + cherryCoin.getBalanceOfAddress(addr1.getPublic('hex')));
+console.log(addr2.getPublic('hex') + ' balance: ' + cherryCoin.getBalanceOfAddress(addr2.getPublic('hex')));
+console.log(addr3.getPublic('hex') + ' balance: ' + cherryCoin.getBalanceOfAddress(addr3.getPublic('hex')));
+console.log(addr4.getPublic('hex') + ' balance: ' + cherryCoin.getBalanceOfAddress(addr4.getPublic('hex')));
 
 console.log(JSON.stringify(cherryCoin, null, 4));
